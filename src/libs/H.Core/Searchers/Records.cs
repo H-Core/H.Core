81234383af54a7de942b4aebd614b3b0aef7ab33
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-29 18:48:35 +1000

    Refactored.

--- src/libs/H.Core/Searchers/Records.cs
+++ src/libs/H.Core/Searchers/Records.cs
@@ -4,16 +4,13 @@
 
 namespace H.Core.Searchers
 {
-
     /// <summary>
     /// Represents a search result from ISearcher's.
     /// </summary>
-    /// <param name="Url">The id of the car.</param>
-    /// <param name="Description">The number of cylinders.</param>
+    /// <param name="Url"></param>
+    /// <param name="Description"></param>
     [Serializable]
-    public record SearchResult(string Url, string Description)
-    {
-    }
+    public record SearchResult(string Url, string Description);
 }
 
 namespace System.Runtime.CompilerServices { internal class IsExternalInit { } }
