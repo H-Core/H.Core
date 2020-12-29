bcb174d8afc5754d38de30360f64e2dd5096dffc
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-29 21:34:22 +1000

    EmptyArray to public.

--- src/libs/H.Core/Utilities/EmptyArray.cs
+++ src/libs/H.Core/Utilities/EmptyArray.cs
@@ -1,14 +1,18 @@
 ﻿namespace H.Core.Utilities
 {
-    internal static class EmptyArray<T>
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class EmptyArray<T>
     {
-        public static T[] Value { get; }
-
-        static EmptyArray()
-        {
+        /// <summary>
+        /// 
+        /// </summary>
 #pragma warning disable CA1825 // Avoid zero-length array allocations
-            Value = new T[0];
+#pragma warning disable CA1000 // Do not declare static members on generic types
+        public static T[] Value { get; } = new T[0];
+#pragma warning restore CA1000 // Do not declare static members on generic types
 #pragma warning restore CA1825 // Avoid zero-length array allocations
-        }
     }
 }
