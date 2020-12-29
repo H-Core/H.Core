e7c1e79e90e70f2ded9acf2642617be5a5601b90
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-29 22:08:58 +1000

    Added Player and Searcher classes.

---
+++ src/libs/H.Core/Players/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using H.Core.Runners;
+
+namespace H.Core.Players
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Player : Runner, IPlayer
+    {
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected Player()
+        {
+            Add(new AsyncAction("play", async (command, cancellationToken) =>
+            {
+                var format = Enum.TryParse<AudioFormat>(
+                    command.Input.Argument, true, out var result) 
+                    ? result
+                    : AudioFormat.Raw;
+                var bytes = command.Input.Data;
+
+                await PlayAsync(bytes, format, cancellationToken).ConfigureAwait(false);
+
+                return Value.Empty;
+            }, "Data: bytes, Arguments: audioFormat"));
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="format"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public abstract Task PlayAsync(
+            byte[] bytes,
+            AudioFormat format = AudioFormat.Raw,
+            CancellationToken cancellationToken = default);
+
+        #endregion
+    }
+}
---
+++ src/libs/H.Core/Searchers/Searcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using H.Core.Runners;
+
+namespace H.Core.Searchers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Searcher : Runner, ISearcher
+    {
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected Searcher()
+        {
+            Add(AsyncAction.WithSingleArgument("search", SearchAsync, "text"));
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public abstract Task<ICollection<SearchResult>> SearchAsync(
+            string text, 
+            CancellationToken cancellationToken = default);
+
+        #endregion
+    }
+}
